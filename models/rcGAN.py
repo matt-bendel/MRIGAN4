ad9cbfcb2e8e9f689be41d184599c5201ba6bfb7
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-01-30 17:17:30 -0500

    Added saving and validation logic

--- models/rcGAN.py
+++ models/rcGAN.py
@@ -295,7 +295,7 @@ class rcGAN(pl.LightningModule):
             avg_psnr = 0.000
 
         self.log('final_val_psnr', avg_psnr, sync_dist=True)
-        self.log('beta_std_mult', self.std_mult)
+        self.log('beta_std_mult', self.std_mult, sync_dist=True)
 
         if self.global_rank == 0:
             send_mail(f"EPOCH {self.current_epoch + 1} UPDATE", f"Metrics:\nPSNR: {np.mean(psnrs):.2f}\nSSIM: {np.mean(ssims):.4f}\nPSNR Diff: {psnr_diff}", file_name="variation_gif.gif")
