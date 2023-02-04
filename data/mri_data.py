f8f19ceb5eb979ed51358f762e5e79671eecdd98
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-02-04 06:44:32 -0500

    Many changes

--- data/mri_data.py
+++ data/mri_data.py
@@ -187,7 +187,7 @@ class SelectiveSliceData_Test(torch.utils.data.Dataset):
             # else:
             #     with h5py.File(pathlib.Path(str(fname).replace('multicoil_val', 'multicoil_val_T2_sense_maps')), 'r') as sense_data:
             #         sense_maps = sense_data['s_maps'][slice]
-            return self.transform(kspace, target, data.attrs, fname.name, slice)
+            return self.transform(kspace, target, data.attrs, fname.name, slice, 0)
 
 
 class SelectiveSliceData(torch.utils.data.Dataset):
@@ -274,7 +274,7 @@ class SelectiveSliceData(torch.utils.data.Dataset):
         with h5py.File(fname, 'r') as data:
             kspace = data['kspace'][slice]
             target = data[self.recons_key][slice] if self.recons_key in data else None
-            return self.transform(kspace, target, data.attrs, fname.name, slice)
+            return self.transform(kspace, target, data.attrs, fname.name, slice, 0)
 
 
 class SelectiveSliceData_Val(torch.utils.data.Dataset):
