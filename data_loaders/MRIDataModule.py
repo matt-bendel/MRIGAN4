75924fadf0afdee4e7997c58fa0700c1d825719b
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-02-13 17:22:38 -0500

    Full res test

--- data_loaders/MRIDataModule.py
+++ data_loaders/MRIDataModule.py
@@ -64,7 +64,7 @@ class MRIDataModule(pl.LightningDataModule):
         return DataLoader(
             dataset=self.train,
             batch_size=self.args.batch_size,
-            num_workers=4,
+            num_workers=112,
             drop_last=True,
             pin_memory=False
         )
@@ -73,7 +73,7 @@ class MRIDataModule(pl.LightningDataModule):
         return DataLoader(
             dataset=self.validate,
             batch_size=self.args.batch_size,
-            num_workers=4,
+            num_workers=112,
             drop_last=True,
             pin_memory=False
         )
