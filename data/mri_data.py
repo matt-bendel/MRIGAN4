b88df252202d12cf572c9288a8f47e4689ce9c9f
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-02-13 13:21:51 -0500

    Full res test

--- data/mri_data.py
+++ data/mri_data.py
@@ -183,15 +183,9 @@ class SelectiveSliceData_Test(torch.utils.data.Dataset):
             target = data[self.recons_key][slice] if self.recons_key in data else None
 
             maps = None
-            with open(f'/storage/fastMRI_brain/sense_maps/test/{fname.name}_{slice}.pkl', 'rb') as inp:
+            with open(f'/storage/fastMRI_brain/sense_maps/test_full_res/{fname.name}_{slice}.pkl', 'rb') as inp:
                 maps = pickle.load(inp)
 
-            # if self.test_set:
-            #     with h5py.File(pathlib.Path(str(fname).replace('small_T2_test', 'small_T2_test_sense_maps')), 'r') as sense_data:
-            #         sense_maps = sense_data['s_maps'][slice]
-            # else:
-            #     with h5py.File(pathlib.Path(str(fname).replace('multicoil_val', 'multicoil_val_T2_sense_maps')), 'r') as sense_data:
-            #         sense_maps = sense_data['s_maps'][slice]
             return self.transform(kspace, target, data.attrs, fname.name, slice, maps)
 
 
@@ -372,15 +366,9 @@ class SelectiveSliceData_Val(torch.utils.data.Dataset):
             target = data[self.recons_key][slice] if self.recons_key in data else None
 
             maps = None
-            with open(f'/storage/fastMRI_brain/sense_maps/val/{fname.name}_{slice}.pkl', 'rb') as inp:
+            with open(f'/storage/fastMRI_brain/sense_maps/val_full_res/{fname.name}_{slice}.pkl', 'rb') as inp:
                 maps = pickle.load(inp)
 
-            # if self.test_set:
-            #     with h5py.File(pathlib.Path(str(fname).replace('small_T2_test', 'small_T2_test_sense_maps')), 'r') as sense_data:
-            #         sense_maps = sense_data['s_maps'][slice]
-            # else:
-            #     with h5py.File(pathlib.Path(str(fname).replace('multicoil_val', 'multicoil_val_T2_sense_maps')), 'r') as sense_data:
-            #         sense_maps = sense_data['s_maps'][slice]
             return self.transform(kspace, target, data.attrs, fname.name, slice, maps)
 
 
