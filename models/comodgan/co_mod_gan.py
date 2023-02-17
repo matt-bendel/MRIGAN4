4c01b74f5645e5bba36e27a008aa1bc54b9da578
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-02-16 22:38:12 -0500

    Uncomment args

--- models/comodgan/co_mod_gan.py
+++ models/comodgan/co_mod_gan.py
@@ -410,18 +410,18 @@ class Discriminator(BaseNetwork):
         y = torch.cat([input, label], 1)
         out = self.convs(y)
         batch, channel, height, width = out.shape
-        group_size = min(batch, 4)
-        stddev = out.view(
-            group_size,
-            -1,
-            1,
-            channel // 1,
-            height, width
-        )
-        stddev = torch.sqrt(stddev.var(0, unbiased=False) + 1e-8)
-        stddev = stddev.mean([2, 3, 4], keepdims=True).squeeze(2)
-        stddev = stddev.repeat(group_size, 1, height, width)
-        out = torch.cat([out, stddev], 1)
+        # group_size = min(batch, 4)
+        # stddev = out.view(
+        #     group_size,
+        #     -1,
+        #     1,
+        #     channel // 1,
+        #     height, width
+        # )
+        # stddev = torch.sqrt(stddev.var(0, unbiased=False) + 1e-8)
+        # stddev = stddev.mean([2, 3, 4], keepdims=True).squeeze(2)
+        # stddev = stddev.repeat(group_size, 1, height, width)
+        # out = torch.cat([out, stddev], 1)
         out = self.Conv4x4(out)
         out = out.view(input.size(0), -1)
         out = self.Dense0(out)
