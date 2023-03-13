653cb9a9c8715b0e788cc705f2e0d66790e19537
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-03-13 01:21:08 -0400

    Trying measured

--- preprocess_mri_data.py
+++ preprocess_mri_data.py
@@ -1,30 +1,59 @@
-import os
-import torch
-import random
-import pickle
 import h5py
-
+import pathlib
+import cv2
 import numpy as np
-import pytorch_lightning as pl
-from torch.utils.data import DataLoader
 
-from pytorch_lightning.callbacks import ModelCheckpoint
-from data_loaders.MRIDataModule import MRIDataModule
-from utils.parse_args import create_arg_parser
-from models.rcGAN import rcGAN
-from pytorch_lightning import seed_everything
-import sigpy as sp
-import sigpy.mri as mr
-from utils.fftc import ifft2c_new, fft2c_new
-from utils.math import complex_abs, tensor_to_complex_np
-from time import time
-import multiprocessing as mp
+from utils.espirit import ifft, fft
+from data import transforms
+
+def reduce_resolution(im):
+    reduced_im = np.zeros((8, 128, 128, 2))
+    for i in range(im.shape[0] // 2):
+        reduced_im[i, :, :, 0] = cv2.resize(im[i, :, :, 0].numpy(), dsize=(128, 128),
+                                            interpolation=cv2.INTER_LINEAR)
+        reduced_im[i, :, :, 1] = cv2.resize(im[i, :, :, 1].numpy(), dsize=(128, 128),
+                                            interpolation=cv2.INTER_LINEAR)
+
+    return reduced_im
+
+
+# Helper functions for Transform
+def flatten(t):
+    t = t.reshape(1, -1)
+    t = t.squeeze()
+    return t
+
+
+def unflatten(t, shape_t):
+    t = t.reshape(shape_t)
+    return t
+
+
+def ImageCropandKspaceCompression(x):
+    w_from = (x.shape[0] - 384) // 2  # crop images into 384x384
+    h_from = (x.shape[1] - 384) // 2
+    w_to = w_from + 384
+    h_to = h_from + 384
+    cropped_x = x[w_from:w_to, h_from:h_to, :]
+    if cropped_x.shape[-1] > 8:
+        x_tocompression = cropped_x.reshape(384 ** 2, cropped_x.shape[-1])
+        U, S, Vh = np.linalg.svd(x_tocompression, full_matrices=False)
+        coil_compressed_x = np.matmul(x_tocompression, Vh.conj().T)
+        coil_compressed_x = coil_compressed_x[:, 0:8].reshape(384, 384, 8)
+    else:
+        coil_compressed_x = cropped_x
+
+    return coil_compressed_x
 
 if __name__ == '__main__':
-    for fname in sorted(files):
-        kspace = h5py.File(fname, 'r')['kspace']
+    root = '/storage/fastMRI_brain/data/multicoil_vaL'
+    files = list(pathlib.Path(root).iterdir())
 
-        if kspace.shape[-1] <= 384 or kspace.shape[1] < 8 or str(
+    for fname in sorted(files):
+        h5_file = h5py.File(fname, 'r')
+        ks = h5_file['kspace']
+
+        if ks.shape[-1] <= 384 or ks.shape[1] < 8 or str(
                 fname) == '/storage/fastMRI_brain/data/multicoil_val/file_brain_AXT2_209_2090296.h5' or str(
             fname) == '/storage/fastMRI_brain/data/multicoil_val/file_brain_AXT2_200_2000250.h5' or str(
             fname) == '/storage/fastMRI_brain/data/multicoil_val/file_brain_AXT2_201_2010106.h5' or str(
@@ -33,10 +62,19 @@ if __name__ == '__main__':
             continue
         else:
             num_slices = 8  # kspace.shape[0]
-            self.examples += [(fname, slice) for slice in range(num_slices)]
 
-    for i, data in enumerate(val_loader):
-        y, x, y_true, mean, std, mask, fname, slice = data
+        new_ksp = np.zeros(kspace.shape)
+        for j in range(kspace.shape[0]):
+            kspace = ks[j].transpose(1, 2, 0)
+            x = ifft(kspace, (0, 1))  # (768, 396, 16)
 
-        for j in range(y.size(0)):
-            hf = h5py.File(f'/storage/fastMRI_brain/preprocessed_data/train/{fname[j]}_{slice[j]}.h5', 'w')
+            coil_compressed_x = ImageCropandKspaceCompression(x)  # (384, 384, 8)
+
+            im_tensor = transforms.to_tensor(coil_compressed_x).permute(2, 0, 1, 3)
+
+            new_ksp[j] = reduce_resolution(im_tensor)
+
+        hf = h5py.File(f'/storage/fastMRI_brain/preprocessed_data/multicoil_val/{fname}.h5', 'w')
+        hf.create_dataset('kspace', new_ksp)
+        hf.create_dataset('reconstruction_rss', h5_file['reconstruction_rss'])
+        hf.close()
