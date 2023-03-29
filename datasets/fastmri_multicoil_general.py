51ff255d5e4a65f5d01d143046eadbf4c41d51e7
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-03-29 01:15:54 -0400

    Replaced no D rcGAN in train

--- datasets/fastmri_multicoil_general.py
+++ datasets/fastmri_multicoil_general.py
@@ -206,9 +206,9 @@ class MulticoilDataset(torch.utils.data.Dataset):
 
         maps = 0
 
-        if self.val:
-            with open(f'/storage/fastMRI_brain/sense_maps/val_full_res/{fname.name}_{dataslice}.pkl', 'rb') as inp:
-                maps = pickle.load(inp)
+        # if self.val:
+        #     with open(f'/storage/fastMRI_brain/sense_maps/val_full_res/{fname.name}_{dataslice}.pkl', 'rb') as inp:
+        #         maps = pickle.load(inp)
 
         if self.test:
             with open(f'/storage/fastMRI_brain/sense_maps/test_full_res/{fname.name}_{dataslice}.pkl', 'rb') as inp:
