4abdf88cce38ece0b0b8b502cb14c2b67126c64c
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-03-29 18:07:05 -0400

    Added wandb logger

--- models/rcGAN.py
+++ models/rcGAN.py
@@ -267,7 +267,7 @@ class rcGAN(pl.LightningModule):
 
         if self.global_rank == 0 and self.current_epoch % 2 == 0:
             send_mail(f"EPOCH {self.current_epoch + 1} UPDATE",
-                      f"Metrics:\nPSNR: {avg_psnr:.2f}\nSINGLE PSNR: {avg_single_psnr:.2f}\nSSIM: {np.mean(ssims):.4f}\nPSNR Diff: {psnr_diff}",
+                      f"Std. Dev. Weight: {self.std_mult:.4f}\nMetrics:\nPSNR: {avg_psnr:.2f}\nSINGLE PSNR: {avg_single_psnr:.2f}\nSSIM: {np.mean(ssims):.4f}\nPSNR Diff: {psnr_diff}",
                       file_name="variation_gif.gif")
 
     def configure_optimizers(self):
