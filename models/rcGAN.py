153f70099c3adb9e9db54ef576be442313317f3a
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-03-29 01:40:21 -0400

    Replaced no D rcGAN in train

--- models/rcGAN.py
+++ models/rcGAN.py
@@ -145,7 +145,7 @@ class rcGAN(pl.LightningModule):
             gens = torch.zeros(
                 size=(y.size(0), self.args.num_z_train, self.args.in_chans, self.args.im_size, self.args.im_size),
                 device=self.device)
-            for z in range(self.args.num_z):
+            for z in range(self.args.num_z_train):
                 gens[:, z, :, :, :] = self.forward(y, mask)
 
             avg_recon = torch.mean(gens, dim=1)
