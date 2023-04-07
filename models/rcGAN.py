37ece3c9b0e8dde882ab58a10fc01a6889278be3
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-04-07 02:45:57 -0400

    Small changes to rcGAN module

--- models/rcGAN.py
+++ models/rcGAN.py
@@ -123,7 +123,7 @@ class rcGAN(pl.LightningModule):
         for k in range(y.shape[0] - 1):
             gen_pred_loss += torch.mean(fake_pred[k + 1])
 
-        adv_weight = 1e-3
+        adv_weight = 1e-4
 
         return adv_weight * gen_pred_loss.mean()
 
