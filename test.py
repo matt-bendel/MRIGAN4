8cbfa69b0168ab79a333f2ef5a723ca6a184f79c
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-04-12 17:39:43 -0400

    Testing...

--- test.py
+++ test.py
@@ -20,7 +20,61 @@ from evaluation_scripts.metrics import psnr, ssim
 from evaluation_scripts.fid.embeddings import VGG16Embedding
 from evaluation_scripts.cfid.cfid_metric import CFIDMetric
 from evaluation_scripts.fid.fid_metric import FIDMetric
+import matplotlib.pyplot as plt
 
+def generate_image(fig, target, image, method, image_ind, rows, cols, kspace=False, disc_num=False):
+    # rows and cols are both previously defined ints
+    ax = fig.add_subplot(rows, cols, image_ind)
+    if method != 'GT' and method != 'Std. Dev':
+        psnr_val = psnr(target, image)
+        snr_val = snr(target, image)
+        ssim_val = ssim(target, image)
+        if not kspace:
+            pred = disc_num
+            ax.set_title(
+                f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}, Pred: {pred * 100:.2f}% True') if disc_num else ax.set_title(
+                f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}')
+
+    if method == 'Std. Dev':
+        im = ax.imshow(image, cmap='viridis')
+        ax.set_xticks([])
+        ax.set_yticks([])
+    else:
+        if kspace:
+            image = image ** 0.4
+            target = target ** 0.4
+        im = ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(target))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlabel(method)
+
+    return im, ax
+
+
+def generate_error_map(fig, target, recon, method, image_ind, rows, cols, relative=False, k=1, kspace=False):
+    # Assume rows and cols are available globally
+    # rows and cols are both previously defined ints
+    ax = fig.add_subplot(rows, cols, image_ind)  # Add to subplot
+
+    # Normalize error between target and reconstruction
+    if kspace:
+        recon = recon ** 0.4
+        target = target ** 0.4
+
+    error = (target - recon) if relative else np.abs(target - recon)
+    # normalized_error = error / error.max() if not relative else error
+    if relative:
+        im = ax.imshow(k * error, cmap='bwr', origin='lower', vmin=-0.0001, vmax=0.0001)  # Plot image
+        plt.gca().invert_yaxis()
+    else:
+        im = ax.imshow(k * error, cmap='jet', vmax=1) if kspace else ax.imshow(k * error, cmap='jet', vmax=0.0001)
+
+    # Remove axis ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    # Return plotted image and its axis in the subplot
+    return im, ax
 
 def load_object(dct):
     return types.SimpleNamespace(**dct)
@@ -72,7 +126,7 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         model = model_alias.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint-epoch=88.ckpt')
+            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint-epoch=0.ckpt')
         model = model.cuda()
         model.eval()
 
@@ -111,6 +165,15 @@ if __name__ == "__main__":
 
                 avg_gen_np = torch.tensor(S.H * avg_ksp).abs().numpy()
                 gt_np = torch.tensor(S.H * gt_ksp).abs().numpy()
+
+                fig = plt.figure()
+
+                generate_image(fig, gt_np, avg_gen_np, f'Recon', 1, 2, 1, disc_num=False)
+                im, ax = generate_error_map(fig, gt_np, avg_gen_np, f'Recon', 2, 2, 1)
+
+                plt.savefig(f'test.png')
+                plt.close()
+                exit()
 
                 for z in range(cfg.num_z_test):
                     np_samp = tensor_to_complex_np((gens[j, z, :, :, :, :] * std[j] + mean[j]).cpu())
