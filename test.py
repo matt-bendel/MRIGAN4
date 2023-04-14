0c4c744964a1e406f826d95f74a9091b72d794a8
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-04-14 00:32:35 -0400

    Testing

--- test.py
+++ test.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
                 plt.close()
 
                 new_y_true = fft2c_new(model.reformat(y)[j] * std[j] + mask[j])
-                maps = mr.app.EspiritCalib(tensor_to_complex_np(new_y_true.cpu()), calib_width=16,
+                maps = mr.app.EspiritCalib(tensor_to_complex_np(new_y_true.cpu()), calib_width=32,
                                            device=sp.Device(0), crop=0.70,
                                            kernel_width=6).run().get()
                 S = sp.linop.Multiply((cfg.im_size, cfg.im_size), maps)
