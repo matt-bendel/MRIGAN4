8db5895c41ecf111422e55fb1fc4510cc72eb289
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-04-14 13:03:05 -0400

    Testing...

--- test.py
+++ test.py
@@ -147,11 +147,9 @@ if __name__ == "__main__":
     with torch.no_grad():
         model = model_alias.load_from_checkpoint(
             checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint-epoch=92.ckpt')
-        # checkpoint_file_gen = pathlib.Path(
-        #     f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/trained_models/generator_best_model.pt')
-        # # checkpoint_file_gen = pathlib.Path(
-        # #     f'{args.checkpoint_dir}/generator_model.pt')
-        # checkpoint_gen = torch.load(checkpoint_file_gen, map_location=torch.device('cuda'))
+        checkpoint_file_gen = pathlib.Path(
+            f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN3/trained_models/generator_best_model.pt')
+        checkpoint_gen = torch.load(checkpoint_file_gen, map_location=torch.device('cuda'))
         #
         # from collections import OrderedDict
         #
@@ -160,10 +158,10 @@ if __name__ == "__main__":
         #     name = k[7:]  # remove `module.`
         #     new_state_dict[name] = v
         #
-        # g = torch.nn.DataParallel(GeneratorModel(18, 16).cuda())
-        # g.load_state_dict(checkpoint_gen['model'])
+        g = torch.nn.DataParallel(GeneratorModel(18, 16).cuda())
+        g.load_state_dict(checkpoint_gen['model'])
 
-        # model.generator = g
+        model.generator = g
         model.cuda()
         model.eval()
 
