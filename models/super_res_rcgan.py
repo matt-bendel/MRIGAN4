82407e37fa28ea4af097f036f73c26f7e9fd3458
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-04-17 00:41:17 -0400

    Super res

--- models/super_res_rcgan.py
+++ models/super_res_rcgan.py
@@ -83,7 +83,7 @@ class SRrcGAN(pl.LightningModule):
     def adversarial_loss_generator(self, gens):
         gen_pred_loss = self.discriminator(gens.reshape(-1, 3, gens.shape[-1], gens.shape[-1]))
 
-        adv_weight = 1e-3
+        adv_weight = 1e-2
 
         return - adv_weight * gen_pred_loss.mean()
 
@@ -115,7 +115,7 @@ class SRrcGAN(pl.LightningModule):
             # adversarial loss is binary cross-entropy
             g_loss = self.adversarial_loss_generator(gens)
             loss, _ = self.perceptual_loss(avg_recon, x)
-            g_loss += 1e-2 * loss
+            g_loss += 1e-1 * loss
             g_loss += self.l1_std_p(avg_recon, gens, x)
 
             if torch.isnan(g_loss):
