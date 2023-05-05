05148e6a75b6dff1c24b5cc262295d7fd8d80ea9
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-05-05 15:39:21 -0400

    Arch changes

--- models/super_res_rcgan.py
+++ models/super_res_rcgan.py
@@ -125,7 +125,7 @@ class SRrcGAN(pl.LightningModule):
 
             for z in range(self.args.num_z_train):
                 loss, _ = self.perceptual_loss(gens[:, z, :, :, :], x)
-                g_loss += 1e-4 * loss / self.args.num_z_train
+                g_loss += 1e-3 * loss / self.args.num_z_train
 
             g_loss += self.l1_std_p(avg_recon, gens, x)
 
