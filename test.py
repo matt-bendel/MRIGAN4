98c04920dd0ae7a11beeaf4d7293063747b49507
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-05-09 21:38:59 -0400

    ESRGAN gen

--- test.py
+++ test.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
         dm.setup()
         test_loader = dm.test_dataloader()
         if args.rcgan:
-            model_alias = Adler
+            model_alias = Ohayon
         else:
             model_alias = L1SSIMMRI
     elif args.inpaint:
