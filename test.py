dcc211bcea25e0e0f7f85f3430d6ea15fd9ed2d1
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-05-22 15:21:36 -0400

    plot change

--- test.py
+++ test.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
         model.cuda()
         model.eval()
 
-        n_samps = [3]
+        n_samps = [1,3,5,9,17,33]
 
         for n in n_samps:
             trial_distss = []
