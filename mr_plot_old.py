ff485f7fdbfa8278a60b7d8dd0ec72b2354f98d3
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-07 19:39:02 -0400

    VarNet Module update

--- mr_plot_old.py
+++ mr_plot_old.py
@@ -280,7 +280,7 @@ if __name__ == "__main__":
                 # TODO: OG fig plot
                 # TODO: metrics
                 # OG FIG
-                nrow = 3
+                nrow = 2
                 ncol = 7
 
                 fig = plt.figure(figsize=(ncol + 1, nrow + 1))
@@ -367,53 +367,53 @@ if __name__ == "__main__":
                 ax.set_xticks([])
                 ax.set_yticks([])
 
-                count = 1
-                for method in keys:
-                    if method != 'l1_ssim' and method != 'varnet':
-                        ax = plt.subplot(gs[2, count])
-                        ax.imshow(np_stds[method], cmap='viridis', vmin=0, vmax=np.max(np_stds['rcgan']))
-                        ax.set_xticklabels([])
-                        ax.set_yticklabels([])
-                        ax.set_xticks([])
-                        ax.set_yticks([])
-                    else:
-                        ax = plt.subplot(gs[2, count])
-                        im = ax.imshow(np.zeros((384, 384)), cmap='viridis', vmin=0, vmax=np.max(np_stds['rcgan']))
-                        ax.set_xticklabels([])
-                        ax.set_yticklabels([])
-                        ax.set_xticks([])
-                        ax.set_yticks([])
-
-                        if method == 'l1_ssim':
-                            # fig.subplots_adjust(right=0.85)  # Make room for colorbar
-
-                            # Get position of final error map axis
-                            [[x10, y10], [x11, y11]] = ax.get_position().get_points()
-
-                            # Appropriately rescale final axis so that colorbar does not effect formatting
-                            pad = 0.01
-                            width = 0.02
-                            cbar_ax = fig.add_axes([x10 - 2 * pad, y10, width, y11 - y10])
-                            cbar = fig.colorbar(im, cax=cbar_ax, format='%.0e',
-                                                orientation='vertical')  # Generate colorbar
-                            cbar.ax.locator_params(nbins=3)
-                            cbar.ax.yaxis.set_ticks_position("left")
-                            cbar.ax.tick_params(labelsize=6)
-                            cbar.ax.tick_params(rotation=0)
-                            tl = cbar.ax.get_yticklabels()
-
-                            # set the alignment for the first and the last
-                            tl[0].set_verticalalignment('bottom')
-                            tl[-1].set_verticalalignment('top')
-
-                    count += 1
-
-                ax = plt.subplot(gs[2, count])
-                ax.imshow(langevin_std, cmap='viridis', vmin=0, vmax=np.max(np_stds['rcgan']))
-                ax.set_xticklabels([])
-                ax.set_yticklabels([])
-                ax.set_xticks([])
-                ax.set_yticks([])
+                # count = 1
+                # for method in keys:
+                #     if method != 'l1_ssim' and method != 'varnet':
+                #         ax = plt.subplot(gs[2, count])
+                #         ax.imshow(np_stds[method], cmap='viridis', vmin=0, vmax=np.max(np_stds['rcgan']))
+                #         ax.set_xticklabels([])
+                #         ax.set_yticklabels([])
+                #         ax.set_xticks([])
+                #         ax.set_yticks([])
+                #     else:
+                #         ax = plt.subplot(gs[2, count])
+                #         im = ax.imshow(np.zeros((384, 384)), cmap='viridis', vmin=0, vmax=np.max(np_stds['rcgan']))
+                #         ax.set_xticklabels([])
+                #         ax.set_yticklabels([])
+                #         ax.set_xticks([])
+                #         ax.set_yticks([])
+                #
+                #         if method == 'l1_ssim':
+                #             # fig.subplots_adjust(right=0.85)  # Make room for colorbar
+                #
+                #             # Get position of final error map axis
+                #             [[x10, y10], [x11, y11]] = ax.get_position().get_points()
+                #
+                #             # Appropriately rescale final axis so that colorbar does not effect formatting
+                #             pad = 0.01
+                #             width = 0.02
+                #             cbar_ax = fig.add_axes([x10 - 2 * pad, y10, width, y11 - y10])
+                #             cbar = fig.colorbar(im, cax=cbar_ax, format='%.0e',
+                #                                 orientation='vertical')  # Generate colorbar
+                #             cbar.ax.locator_params(nbins=3)
+                #             cbar.ax.yaxis.set_ticks_position("left")
+                #             cbar.ax.tick_params(labelsize=6)
+                #             cbar.ax.tick_params(rotation=0)
+                #             tl = cbar.ax.get_yticklabels()
+                #
+                #             # set the alignment for the first and the last
+                #             tl[0].set_verticalalignment('bottom')
+                #             tl[-1].set_verticalalignment('top')
+                #
+                #     count += 1
+                #
+                # ax = plt.subplot(gs[2, count])
+                # ax.imshow(langevin_std, cmap='viridis', vmin=0, vmax=np.max(np_stds['rcgan']))
+                # ax.set_xticklabels([])
+                # ax.set_yticklabels([])
+                # ax.set_xticks([])
+                # ax.set_yticks([])
 
                 plt.savefig(f'mr_figs/body_mri_fig_avg_err_std_{fig_count}.png', bbox_inches='tight', dpi=300)
 
