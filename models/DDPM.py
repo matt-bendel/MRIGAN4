15241019790b5e0a543528ac5b4fa01f37d0bc63
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-28 15:52:24 -0400

    Code to train MRI ddpm

---
+++ models/DDPM.py
@@ -0,0 +1,79 @@
+import torch
+
+import pytorch_lightning as pl
+import numpy as np
+
+from PIL import Image
+from utils.fftc import ifft2c_new, fft2c_new
+from utils.math import complex_abs, tensor_to_complex_np
+from mail import send_mail
+from denoising_diffusion_pytorch import Unet, GaussianDiffusion
+
+class DDPM(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.ddpm_net = Unet(
+            dim=128,
+            dim_mults=(1, 1, 2, 2, 4, 4),
+            channels=2,
+            resnet_block_groups=2,
+            attn_dim_head=32
+        )
+        self.diffusion = GaussianDiffusion(
+            self.ddpm_net,
+            image_size=384,
+            timesteps=1000
+        )
+
+        self.resolution = self.args.im_size
+
+        self.save_hyperparameters()  # Save passed values
+
+    def forward(self):
+        pass
+
+    def training_step(self, batch, batch_idx):
+        x = batch
+
+        # train generator
+        loss = self.diffusion(x)
+
+        self.log('diff_loss', loss, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx, external_test=False):
+        x = batch
+
+        # TODO: Plot as tensors using torch function
+        if batch_idx == 0:
+            samps = self.diffusion.sample(batch_size=4)
+            samps = complex_abs(ifft2c_new(samps.permute(0, 2, 3, 1)))
+
+            for i in range(4):
+                samps[i, :, :] = (samps[i, :, :] - samps[i, :, :].min()) / (samps[i, :, :].max() - samps[i, :, :].min())
+
+            if self.global_rank == 0 and self.current_epoch % 5 == 0:
+                self.logger.log_image(
+                    key=f"epoch_{self.current_epoch}_img",
+                    images=[Image.fromarray(np.uint8(samps[0, :, :].cpu().numpy()*255), 'L'), Image.fromarray(np.uint8(samps[1, :, :].cpu().numpy()*255), 'L'), Image.fromarray(np.uint8(samps[2, :, :].cpu().numpy()*255)), Image.fromarray(np.uint8(samps[3, :, :].cpu().numpy()*255))],
+                    caption=["Samp 1", "Samp 2", "Samp 3", "Samp 4"]
+                )
+
+            self.trainer.strategy.barrier()
+
+        return {}
+
+    def validation_epoch_end(self, validation_step_outputs):
+        if self.global_rank == 0 and self.current_epoch % 1 == 0:
+            send_mail(f"EPOCH {self.current_epoch + 1} UPDATE - DDPM",
+                      f"TEMP",
+                      file_name="variation_gif.gif")
+
+        self.trainer.strategy.barrier()
+
+    def configure_optimizers(self):
+        opt_g = torch.optim.Adam(self.ddpm_net.parameters(), lr=5e-5, weight_decay=0)
+
+        return opt_g
+
---
+++ train_diffusion.py
@@ -0,0 +1,67 @@
+import torch
+import os
+import yaml
+import types
+import json
+
+import pytorch_lightning as pl
+
+from pytorch_lightning.callbacks import ModelCheckpoint
+from data_loaders.MRIDataModuleDiffusion import MRIDataModuleDiffusion
+from utils.parse_args import create_arg_parser
+from models.DDPM import DDPM
+from models.l1_ssim_module import L1SSIMMRI
+from pytorch_lightning import seed_everything
+from pytorch_lightning.loggers import WandbLogger
+
+def load_object(dct):
+    return types.SimpleNamespace(**dct)
+
+if __name__ == '__main__':
+    torch.set_float32_matmul_precision('medium')
+    args = create_arg_parser().parse_args()
+    seed_everything(0, workers=True)
+
+    print(f"Experiment Name: {args.exp_name}")
+    print(f"Number of GPUs: {args.num_gpus}")
+
+    with open('configs/mri/config.yml', 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+        cfg = json.loads(json.dumps(cfg), object_hook=load_object)
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_psnr',
+        mode='max',
+        dirpath=cfg.checkpoint_dir + args.exp_name + '/',
+        filename='best_model',
+        save_top_k=1
+    )
+
+    if args.dp:
+        cfg.batch_size = cfg.batch_size * args.num_gpus
+
+    dm = MRIDataModuleDiffusion()
+    # dm = FastMRIDataModule(base_path=cfg.data_path, batch_size=cfg.batch_size, num_data_loader_workers=2 if not args.dp else 20)
+
+    model = DDPM()
+
+    wandb_logger = WandbLogger(
+        project="neurips",
+        name=args.exp_name,
+        log_model="all",
+        save_dir=cfg.checkpoint_dir + 'wandb'
+    )
+    checkpoint_callback_epoch = ModelCheckpoint(
+        monitor='epoch',
+        mode='max',
+        dirpath=cfg.checkpoint_dir + args.exp_name + '/',
+        filename='checkpoint-{epoch}',
+        save_top_k=1
+    )
+
+    trainer = pl.Trainer(accelerator="gpu", devices=args.num_gpus, strategy='ddp',
+                         max_epochs=50, callbacks=[checkpoint_callback_epoch],
+                         num_sanity_val_steps=0, profiler="simple", logger=wandb_logger, benchmark=False, log_every_n_steps=10)
+
+    trainer.fit(model, dm)
+
