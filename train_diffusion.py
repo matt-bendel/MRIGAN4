74935aca8c3601755d4220d39c7038e002942a5c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-28 15:53:57 -0400

    Code to train MRI ddpm

--- train_diffusion.py
+++ train_diffusion.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
         save_top_k=1
     )
 
-    trainer = pl.Trainer(accelerator="gpu", devices=args.num_gpus, strategy='ddp',
+    trainer = pl.Trainer(accelerator="gpu", devices=4, strategy='ddp',
                          max_epochs=50, callbacks=[checkpoint_callback_epoch],
                          num_sanity_val_steps=0, profiler="simple", logger=wandb_logger, benchmark=False, log_every_n_steps=10)
 
