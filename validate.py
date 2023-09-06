f39293a7543c3b4ce6981408f9873724bd528982
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-09-05 23:57:57 -0400

    Account for no DC

--- validate.py
+++ validate.py
@@ -41,8 +41,8 @@ if __name__ == "__main__":
     best_epoch = -1
     inception_embedding = VGG16Embedding()
     best_cfid = 10000000
-    start_epoch = 80
-    end_epoch = 100
+    start_epoch = 130
+    end_epoch = 150
 
     with torch.no_grad():
         for epoch in range(start_epoch, end_epoch):
