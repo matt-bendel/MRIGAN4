e57288947195d2778ef6d1328304833f945bab66
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-09-25 12:37:02 -0400

    Debugging

--- test_ag.sh
+++ test_ag.sh
@@ -1,15 +1,15 @@
 # GRO Tests
 echo "R=2 Models - GRO" > "2.txt"
 echo "Blind No DC" >> "2.txt"
-python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_bline_no_dc --nodc --mask-type 1 --R 2 >> "2.txt"
+python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_blind_no_dc --nodc --mask-type 1 --R 2 >> "2.txt"
 
 echo "R=4 Models - GRO" > "4.txt"
 echo "Blind No DC" >> "4.txt"
-python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_bline_no_dc --nodc --mask-type 1 --R 4 >> "4.txt"
+python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_blind_no_dc --nodc --mask-type 1 --R 4 >> "4.txt"
 
 echo "R=6 Models - GRO" > "6.txt"
 echo "Blind No DC" >> "6.txt"
-python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_bline_no_dc --nodc --mask-type 1 --R 6 >> "6.txt"
+python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_blind_no_dc --nodc --mask-type 1 --R 6 >> "6.txt"
 
 #echo "Baseline UNet" >> "4.txt"
 #python test.py --mri --exp-name neurips/l1_ssim_agnostic --mask-type 1 --R 4 >> "4.txt"
@@ -19,7 +19,7 @@ python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_bline_no_dc --nod
 #
 echo "R=8 Models - GRO" > "8.txt"
 echo "Blind No DC" >> "8.txt"
-python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_bline_no_dc --nodc --mask-type 1 --R 8 >> "8.txt"
+python test.py --mri --rcgan --exp-name neurips/rcgan_agnostic_blind_no_dc --nodc --mask-type 1 --R 8 >> "8.txt"
 
 #echo "Baseline UNet" >> "8.txt"
 #python test.py --mri --exp-name neurips/l1_ssim_agnostic --mask-type 1 --R 8 >> "8.txt"
