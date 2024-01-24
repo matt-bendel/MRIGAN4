b006a7801697f922fae3b8f1fb52a025515cd73b
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-01-24 09:01:38 -0500

    CFID test

--- evaluation_scripts/fid/fid_metric.py
+++ evaluation_scripts/fid/fid_metric.py
@@ -287,7 +287,7 @@ class FIDMetric:
 
             with torch.no_grad():
                 for j in range(condition.shape[0]):
-                    new_y_true = fft2c_new(self.gan.reformat(condition) * std[j] + mean[j])
+                    new_y_true = fft2c_new(self.gan.reformat(condition)[j] * std[j] + mean[j])
                     s_maps = mr.app.EspiritCalib(tensor_to_complex_np(new_y_true.cpu()), calib_width=16,
                                                  device=sp.Device(0), show_pbar=False, crop=0.70,
                                                  kernel_width=6).run().get()
