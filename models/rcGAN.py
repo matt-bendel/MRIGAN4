12050f9cc4cc4e32ae1708cde2f2e32daf8f84cc
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-02-06 23:39:53 -0500

    CFID test

--- models/rcGAN.py
+++ models/rcGAN.py
@@ -1371,10 +1371,10 @@ class EigenGANPix(pl.LightningModule):
 
             if (self.global_step - 1) % self.args.pca_reg_freq == 0 and self.current_epoch >= 0:
                 gens_embed = torch.zeros(
-                    size=(y.size(0), self.args.num_z_pca, 4718592),
+                    size=(y.size(0), self.args.num_z_pca, 2359296),
                     device=self.device)
                 for z in range(self.args.num_z_pca):
-                    gens_embed[:, z, :] = self.forward(y, mask).view(y.size(0), self.args.num_z_pca, -1)
+                    gens_embed[:, z, :] = self.forward(y, mask).view(y.size(0), -1)
 
                 gens_zm = gens_embed - torch.mean(gens_embed, dim=1)[:, None, :].clone().detach()
                 gens_zm = gens_zm.view(gens_embed.shape[0], self.args.num_z_pca, -1)
