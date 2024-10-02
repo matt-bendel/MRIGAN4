3991e7803d24a48c081c776bdb3fbf83884eb702
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-10-02 11:06:51 -0400

    Added ohayon to plot

--- mr_plot_old.py
+++ mr_plot_old.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
 
                 np_samps = {
                     'rcgan': [],
-                    'ohayon': None,
+                    'ohayon': [],
                     'eigengan': [],
                     'adler': []
                 }
